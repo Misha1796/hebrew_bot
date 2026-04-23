878a803dd1ace1700d477509ae41cd0437d63036
Misha1796 <150789896+Misha1796@users.noreply.github.com>
2026-04-23 13:28:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ GEMINI_KEY = os.getenv("GEMINI_KEY")
 
 # Настройка Gemini (используем обновленный вызов модели)
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
